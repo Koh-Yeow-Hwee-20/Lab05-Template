01d5d18f988867a92815a2f792cec23aba72d488
YeowHwee <yeowhwee13@gmail.com>
2022-01-27 16:51:56 +0800

    Add collision scripts for coin n water

---
+++ Assets/Standard Assets/Characters/FirstPersonCharacter/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    //public Gameobject Coins;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //Collision for Coins
+        if (other.gameObject.tag == "Coin")
+        {
+            //coinCount++;
+            //Coins.GetComponent<Text>().text = "Coins Collected = " + coinCount;
+
+            Destroy(other.gameObject);
+        }
+
+        if (other.gameObject.tag == "Water")
+        {
+            SceneManager.LoadScene("GameScene");
+        }
+
+    }
+}
