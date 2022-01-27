819a10851b1d1b8bfb8535deab0290f81b8a3b41
YeowHwee <yeowhwee13@gmail.com>
2022-01-27 17:27:03 +0800

    Added Timer

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,9 +6,16 @@ using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
+    //Coin and Score
     public GameObject Coins;
     private int coinCount;
     int Totalcoin = 60;
+
+    //Time
+    public float timeleft;
+    public int timeRemaining;
+    public Text Timertext;
+    private float TimerValue;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +26,17 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        timeleft -= Time.deltaTime;
+        timeRemaining = Mathf.FloorToInt(timeleft % 60);
+        Timertext.text = "Time: " + timeRemaining.ToString();
+
         if (coinCount == Totalcoin)
         {
             SceneManager.LoadScene("WinScene");
+        }
+        else if (timeleft <= 0.5f)
+        {
+            SceneManager.LoadScene("LoseScene");
         }
     }
     private void OnTriggerEnter(Collider other)
